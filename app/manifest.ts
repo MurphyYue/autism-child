81a4848622cfcbdc0aeb6f6dcd8304a6e7c921da
murphyyue <murphyyue@icloud.com>
2025-05-01 11:40:54 +0800

    feat: add manifest file for PWA configuration
    Introduce a new manifest file to define the Progressive Web App (PWA) metadata, including app name, description, icons, and display settings. This enables the app to be installed and run as a standalone application on supported devices.

---
+++ app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'star cat',
+    short_name: 'star cat',
+    description: 'communicate with your simulated star cat',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    // theme_color: '#000000',
+    icons: [
+      {
+        src: '/logo-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable'
+      },
+      {
+        src: '/logo-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable'
+      },
+    ],
+  }
+}
